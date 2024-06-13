b2626112a29b43c4fb7861e8aa34ef4788e70796
ingeniero79 <pergag@gmail.com>
2024-06-13 12:00:21 +0200

    updated files and new files added

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import PortfolioItem from "./portfolio-item";
 
@@ -9,19 +10,11 @@ export default class PortfolioContainer extends Component {
     this.state = {
       pageTitle: "Welcome to my portfolio",
       isLoading: false,
-      data: [
-        { title: "Quip", category: "eCommerce", slug: "quip" },
-        { title: "Eventbrite", category: "Scheduling", slug: "eventbrite" },
-        {
-          title: "Ministry Safe",
-          category: "Enterprise",
-          slug: "ministry-safe"
-        },
-        { title: "SwingAway", category: "eCommerce", slug: "swingaway" }
-      ]
+      data: []
     };
 
     this.handleFilter = this.handleFilter.bind(this);
+    
   }
 
   handleFilter(filter) {
@@ -32,19 +25,53 @@ export default class PortfolioContainer extends Component {
     });
   }
 
+  getPortfolioItems(){
+    axios.get('https://elingeniero.devcamp.space/portfolio/portfolio_items')
+  .then(response => {
+    // handle success
+    console.log("response data", response);
+    this.setState({
+      data: response.data.portfolio_items
+    })
+  })
+  
+  .catch(error => {
+    // handle error
+    console.log(error);
+  });
+  // version actualizada de axios
+  // .then(function ("response data", response) {
+  //   // handle success
+  //   console.log(response);
+  // })
+  // .catch(function (error) {
+  //   // handle error
+  //   console.log(error);
+  // })
+  // .finally(function () {
+  //   // always executed
+  // }); 
+  }
+
   portfolioItems() {
-    return this.state.data.map(item => {
+
+    
+    return this.state.data.map(item => { 
       return (
-        <PortfolioItem title={item.title} url={"google.com"} slug={item.slug} />
+        <PortfolioItem key={item.id} item={item} />
       );
     });
   }
 
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
   render() {
     if (this.state.isLoading) {
       return <div>Loading...</div>;
     }
-
+    
+    
     return (
       <div>
         <h2>{this.state.pageTitle}</h2>
