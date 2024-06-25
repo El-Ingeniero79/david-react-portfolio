7193b9ac86580a789731ab01cbf5b1e38a5d5419
ingeniero79 <pergag@gmail.com>
2024-06-25 11:14:36 +0200

    updated files

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -5,54 +5,69 @@ import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list";
 import PortfolioForm from "../portfolio/portfolio-form";
 
 export default class PortfolioManager extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            portfolioItems: []
+    this.state = {
+      portfolioItems: []
+    };
+
+    this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(
+      this
+    );
+    this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
+  }
+
+  handleSuccessfulFormSubmission(portfolioItem) {
+    this.setState({
+      portfolioItems: [portfolioItem].concat(this.state.portfolioItems)
+    });
+  }
+
+  handleFormSubmissionError(error) {
+    console.log("handleFormSubmissionError error", error);
+  }
+
+  getPortfolioItems() {
+    axios
+      .get(
+        "https://elingeniero.devcamp.space/portfolio/portfolio_items?order_by=created_at&direction=desc",
+        {
+          withCredentials: true
         }
-        this.handleSucessfulFormSubmission = this.handleSucessfulFormSubmission.bind(this);
-        this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
-    }
-    handleSucessfulFormSubmission(PortfolioItem) {
+      )
+      .then(response => {
+        this.setState({
+          portfolioItems: [...response.data.portfolio_items]
+        });
+      })
+      .catch(error => {
+        console.log("error in getPortfolioItems", error);
+      });
+  }
 
-    } 
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
 
-    handleFormSubmissionError(error) {
-        console.log("handleFormSubmissionError error", error)
-    }
+  render() {
+    return (
+      <div className="portfolio-manager-wrapper">
+        <div className="left-column">
+          <PortfolioForm
+            handleSuccessfulFormSubmission={this.handleSuccessfulFormSubmission}
+            handleFormSubmissionError={this.handleFormSubmissionError}
+          />
+        </div>
 
-    getPortfolioItems() {
-        axios.get("https://elingeniero.devcamp.space/portfolio/portfolio_items",{
-            withCredentials: true
-        }).then(response => {
-            this.setState({
-                portfolioItems: [...response.data.portfolio_items]
-            })
-            
-            
-        }).catch(error => {
-            console.log("error in getPortfolioItems", error)
-        })
-    }
-    componentDidMount() {
-        this.getPortfolioItems();
-    }
-    render() {
-        return (
-            <div className="portfolio-manager-wrapper">
-                <div className="left-column">
-                    <PortfolioForm 
-                        handleSucessfulFormSubmission = {this.handleSucessfulFormSubmission}
-                        handleFormSubmissionError = {this.handleFormSubmissionError}
-                        />
-                </div>
-                
-                <div className="right-column">
-                    <PortfolioSidebarList data={this.state.portfolioItems}/>
-                </div>
-
-            </div>
-        );
-    }
+        <div className="right-column">
+          <PortfolioSidebarList data={this.state.portfolioItems} />
+        </div>
+      </div>
+    );
+  }
 }
+
+
+
+
