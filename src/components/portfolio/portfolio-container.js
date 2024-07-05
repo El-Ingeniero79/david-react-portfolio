80ad3b2ac84e7d48845001c7cbc6c6b56fd04e4a
ingeniero79 <pergag@gmail.com>
2024-07-05 10:15:55 +0200

    new files added and updated

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -14,7 +14,6 @@ export default class PortfolioContainer extends Component {
     };
 
     this.handleFilter = this.handleFilter.bind(this);
-    
   }
 
   handleFilter(filter) {
@@ -25,53 +24,34 @@ export default class PortfolioContainer extends Component {
     });
   }
 
-  getPortfolioItems(){
-    axios.get('https://elingeniero.devcamp.space/portfolio/portfolio_items')
-  .then(response => {
-    // handle success
-    console.log("response data", response);
-    this.setState({
-      data: response.data.portfolio_items
-    })
-  })
-  
-  .catch(error => {
-    // handle error
-    console.log(error);
-  });
-  // version actualizada de axios
-  // .then(function ("response data", response) {
-  //   // handle success
-  //   console.log(response);
-  // })
-  // .catch(function (error) {
-  //   // handle error
-  //   console.log(error);
-  // })
-  // .finally(function () {
-  //   // always executed
-  // }); 
+  getPortfolioItems() {
+    axios
+      .get("https://elingeniero.devcamp.space/portfolio/portfolio_items")
+      .then(response => {
+        this.setState({
+          data: response.data.portfolio_items
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   portfolioItems() {
-
-    
-    return this.state.data.map(item => { 
-      return (
-        <PortfolioItem key={item.id} item={item} />
-      );
+    return this.state.data.map(item => {
+      return <PortfolioItem key={item.id} item={item} />;
     });
   }
 
   componentDidMount() {
     this.getPortfolioItems();
   }
+
   render() {
     if (this.state.isLoading) {
       return <div>Loading...</div>;
     }
-    
-    
+
     return (
       <div className="portfolio-items-wrapper">
         <button className="btn" onClick={() => this.handleFilter("eCommerce")}>
@@ -85,8 +65,10 @@ export default class PortfolioContainer extends Component {
         </button>
 
         {this.portfolioItems()}
-        
       </div>
     );
   }
 }
+
+
+
